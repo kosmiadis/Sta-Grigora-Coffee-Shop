59285a31297defa865e11ec5422b2136cc3b463b
VagelisKosmiadis <vageliskosmiadis@gmail.com>
2022-08-06 12:59:39 +0300

    Will complete later

--- JS/forms.js
+++
@@ -1,7 +0,0 @@
-const submitBtn = document.querySelector('.submit')
-const inputs = document.querySelectorAll('input')
-const textareas = document.querySelectorAll('textarea')
-submitBtn.addEventListener('click', e => {
-    inputs.forEach(input => input.value = '')
-    textareas.forEach(textarea => textarea.value = '')
-})
