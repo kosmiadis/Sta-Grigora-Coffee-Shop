78c2c4075aaf970ad9249550b3abb9774a394885
VagelisKosmiadis <vageliskosmiadis@gmail.com>
2022-08-14 15:34:36 +0300

    Added new Lines

--- JS/coffees.js
+++ JS/coffees.js
@@ -2,7 +2,7 @@ const data = [
     {coffeeName: "Espresso Freddo Micorfarm", price: '2.20', imcSRC: 'images/coffees/EspressoFreddoMicrofarm.png'},
     {coffeeName: "Espressone Praline Microfarm", price: '3.30', imcSRC: 'images/coffees/EspressonePralineMicrofarm.png'},
     {coffeeName: "Espresso Tonic Microfarm", price: '2.80', imcSRC: 'images/coffees/EspressoTonicMicrofarm.png'},
-    {coffeeName: "Freddo Cappuccino με φυτική κρέμα", price: '2.40', imcSRC: 'images/coffees/FreddoCappuccinomeFutikiKremaMicrofarm.png'},
+    {coffeeName: "Freddo Cappuccino", price: '2.40', imcSRC: 'images/coffees/FreddoCappuccinomeFutikiKremaMicrofarm.png'},
     {coffeeName: "Iced Latte Microfarm", price: '2.70', imcSRC: 'images/coffees/IcedLatteMicrofarm.png'},
     {coffeeName: "Iced Spanish Latte Microfarm", price: '2.90', imcSRC: 'images/coffees/IcedSpanishLatteMicrofarm.png'}
 ]
